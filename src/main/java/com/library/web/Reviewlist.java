23e2128aa2ea46c0ad5a143753ab14703cd42eb1
admin <admin@DESKTOP-LNMVMLI>
2024-02-02 14:45:08 +0900

    Reviewlist 서블릿 수정

--- src/main/java/com/library/web/Reviewlist.java
+++ src/main/java/com/library/web/Reviewlist.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.library.dao.ReviewDAO;
+
 @WebServlet("/reviewlist")
 public class Reviewlist extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -36,14 +38,21 @@ public class Reviewlist extends HttpServlet {
   }
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.setContentType("application/json");
-    response.setCharacterEncoding("UTF-8");
+	  response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
 
-    String sampleReceive = request.getParameter("sample_give");
-    System.out.println(sampleReceive);
+      // 클라이언트로부터 전달된 데이터 받기
+      String title = request.getParameter("title_give");
+      String author = request.getParameter("author_give");
+      String reviewContent = request.getParameter("review_give");
 
-    PrintWriter out = response.getWriter();
-    out.print("{\"msg\": \"이 요청은 POST!\"}");
-    out.flush();
+      // 받은 데이터를 데이터베이스에 저장
+      ReviewDAO reviewDAO = new ReviewDAO();
+      reviewDAO.addReview(title, author, reviewContent);
+
+      // 클라이언트로 응답 보내기
+      PrintWriter out = response.getWriter();
+      out.print("{\"msg\": \"리뷰가 성공적으로 저장되었습니다.\"}");
+      out.flush();
   }
 }
