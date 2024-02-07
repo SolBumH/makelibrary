bf23d4d6d27661805b417a5585c369709392f24c
DESKTOP-ECIUREE\admin <jhj4591@naver.com>
2024-02-07 12:11:16 +0900

    효진 커밋

--- src/main/java/com/library/dao/AdminDAO.java
+++ src/main/java/com/library/dao/AdminDAO.java
@@ -75,104 +75,13 @@ public class AdminDAO extends AbstractDAO {
 		return list;
 	}
 
+
 	// 레벨업
 	public int memberUpdate(int grade, int mno) {
-
 		Connection con = db.getConnection();
 		PreparedStatement pstmt = null;
 		String sql = "UPDATE member SET mgrade=? WHERE mno=?";
 		int result = 0;
-
-		try {
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, grade);
-			pstmt.setInt(2, mno);
-			result = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(null, pstmt, con);
-		}
-		return result;
-	}
-
-	// 계정 등급 관리
-	public List<MemberDTO> memberList(int grade) {
-
-		List<MemberDTO> list = new ArrayList<MemberDTO>();
-		Connection con = db.getConnection();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = "SELECT mno, mid, mname, mdate, mgrade FROM member WHERE mgrade=?";
-
-		try {
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, grade);
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				MemberDTO e = new MemberDTO();
-				e.setMno(rs.getInt("mno"));
-				e.setMid(rs.getString("mid"));
-				e.setMname(rs.getString("mname"));
-				e.setMdate(rs.getString("mdate"));
-				e.setMgrade(rs.getInt("mgrade"));
-				list.add(e);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(rs, pstmt, con);
-		}
-
-		return list;
-	}
-
-	public List<Map<String, Object>> allRent() {
-		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		Connection con = db.getConnection();
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = "SELECT btitle, mid, rtenum, rtdate FROM rentlist " + "LIMIT 0, 10";
-
-		try {
-			pstmt = con.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				Map<String, Object> e = new HashMap<String, Object>();
-				e.put("btitle", rs.getString("btitle"));
-				e.put("mid", rs.getString("mid"));
-				e.put("rtenum", rs.getString("rtenum"));
-				e.put("rtdate", rs.getString("rtdate"));
-
-			while (rs.next()) {
-				ReviewDTO e = new ReviewDTO();
-				e.setRno(rs.getInt("rno"));
-				e.setMno(rs.getInt("mno"));
-				e.setRtitle(rs.getString("rtitle"));
-				e.setRauthor(rs.getString("rauthor"));
-				e.setRcontent(rs.getString("rcontent"));
-				e.setRdate(rs.getDate("rdate"));
-				e.setRdel(rs.getString("rdel"));
-				list.add(e);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(rs, pstmt, con);
-		}
-		return list;
-	}
-
-	// 레벨업
-	public int memberUpdate(int grade, int mno) {
-
-		Connection con = db.getConnection();
-		PreparedStatement pstmt = null;
-		String sql = "UPDATE member SET mgrade=? WHERE mno=?";
-		int result = 0;
-
 		try {
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, grade);
